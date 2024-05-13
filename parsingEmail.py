3cd7e2f414f8db325b04187ed9c4341fd3670698
younes-t <younes3046@gmail.com>
2024-05-13 05:59:49 -0400

    parsing webpages for email

---
+++ parsingEmail.py
@@ -0,0 +1,29 @@
+
+import re
+import requests
+import smtplib
+from email.mime.text import MIMEText
+
+
+#[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}
+def find_mail(ch:str):
+    regex_email = r"([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)"
+    return re.findall(regex_email, ch)
+
+def get_sourcecode(url:str):
+    try: 
+        response = requests.get(url)
+        if response.status_code == 200:
+            html_content = response.text
+            return html_content
+        return None
+    except Exception as e:
+        print("Erreur:", e)
+        return None
+
+def parsing_webpage(url:str):
+    return find_mail(get_sourcecode(url))
+
+def main():
+    print(parsing_webpage("https://www.oteria.fr"))
+main()
